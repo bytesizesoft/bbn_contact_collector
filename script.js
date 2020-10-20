4d419a6b8bac143648e408526dc6f75dd8e64d6c
Adebisi Ayomide <Adebisiayomide07@gmail.com>
2020-10-20 15:38:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = [...document.querySelectorAll(".step p")];
+const progressCheck = [...document.querySelectorAll(".step .check")];
+const bullet = [...document.querySelectorAll(".step .bullet")];
+let max = 4;
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("You Have Successfully Submitted Your Details");
+    location.reload();
+  },800);
+});
+
+prevBtnSec.addEventListener("click", function(){
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+
+function verify_msisdn() {
+  // send otp to the users msisdn
+  user_msisdn = document.getElementById("msisdn").value
+  user_network = document.getElementById("network").value
+  api_endpoint = "http://207.154.239.226:8083/send_sms/" + user_msisdn.toString() +"/"+ user_network
+  data = {
+    "msisdn":user_msisdn,
+    "network" : user_network
+  }
+  fetch(api_endpoint, {
+    method: 'post',
+    body: JSON.stringify(data)
+  }).then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  console.log(user_msisdn)
+  console.log(user_network)
+  console.log("sms sent successfully")
+  
+}
